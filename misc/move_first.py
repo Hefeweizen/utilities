c178e06673ff30394ef92b0eb8336a291d474a9e
Hefeweizen <jmales@gmail.com>
2023-07-03 13:38:46 -0400

    refactor move_first

--- misc/move_first.py
+++ misc/move_first.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
 # in a list, move arg to first position, preserving order
+#
+# The description above is the effect.  The implementation
+# is slightly different:  buffer until a line is found, print
+# the line, print the buffer, and then just pass everything
+# onward.
 
 import sys
 
 
 def main():
-    try:
+    # sys.argv will always have at least one element
+    if len(sys.argv) > 1:
         target = sys.argv[1].lower()
-    except IndexError:
-        # no arg specified so short-circuit and return everything
-        sys.stdout.write(sys.stdin.read())
-        # exit for completeness; as stdin is now empty letting the
-        # rest of the program would do nothing.
-        sys.exit()
+        buffer = []
+        for line in sys.stdin:
+            if target not in line.lower():
+                buffer.append(line)
+            else:
+                # print first item found
+                sys.stdout.write(line)
 
-    source_list = []
-    for line in map(str.strip, sys.stdin):
-        source_list.append(line)
+                # print buffer
+                for buffer_line in buffer:
+                    sys.stdout.write(buffer_line)
 
-    for idx, value in enumerate(source_list):
-        if target in value.lower():
-            source_list.insert(0, source_list[idx])
-            source_list.pop(idx+1)
-            break  # stop looking
+                # cleanup
+                del buffer
+                break
 
-    for line in source_list:
-        print(line)
+    # pass everything forward
+    sys.stdout.write(sys.stdin.read())
 
 
 if __name__ == "__main__":
