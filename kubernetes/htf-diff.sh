06da099dd14a6b29b0eada08be81eff4a0bd44b0
Hefeweizen <jmales@gmail.com>
2025-07-23 22:49:33 -0700

    htf-diff: hydrate current branch, main and then diff

---
+++ kubernetes/htf-diff.sh
@@ -0,0 +1,20 @@
+#!/opt/homebrew/bin/bash
+
+# foo.<sha> -- use short sha as file extension
+local_foo="foo.$(git rev-parse @ | cut -c 1-8)"
+
+# add check that we're in a good dir
+# i.e. there's a Chart.yaml
+if [ ! -f Chart.yaml ]; then
+    echo "bad location; no local chart"
+    exit 1
+fi
+
+git stash push
+git switch main
+htf --base -o foo.main
+git switch -
+git stash pop
+htf --no-clean --no-deps --base -o ${local_foo}
+
+diff foo.main  ${local_foo}
